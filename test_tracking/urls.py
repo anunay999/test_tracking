e8475049b550885a61bd1fd5e527ec409e56df9c
anunay999 <anunay1997@gmail.com>
2020-02-14 18:31:01 +0530

    added module page and edit page

--- test_tracking/urls.py
+++ test_tracking/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('test_cases.urls'))
+    path('', include('test_cases.urls'))
 ]
