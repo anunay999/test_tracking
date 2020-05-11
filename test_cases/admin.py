53ecf2da19780be9002b1f5a8daff2d57a4ac9f4
anunay999 <anunay1997@gmail.com>
2020-05-11 22:21:04 +0530

    make it better

--- test_cases/admin.py
+++ test_cases/admin.py
@@ -3,6 +3,4 @@ from .models import Tracker
 #from import_export.admin import ImportExportModelAdmin
 from . import models
 # Register your models here.
-@admin.register(Tracker)
-class ViewAdmin(ImportExportModelAdmin):
-    pass
+
