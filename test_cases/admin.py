6310f0fdd44776edab09b82c856fdec4304ecdbe
anunay999 <anunay1997@gmail.com>
2020-05-11 21:54:19 +0530

    make it better

--- test_cases/admin.py
+++ test_cases/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Tracker
-from import_export.admin import ImportExportModelAdmin
+#from import_export.admin import ImportExportModelAdmin
 from . import models
 # Register your models here.
 @admin.register(Tracker)
